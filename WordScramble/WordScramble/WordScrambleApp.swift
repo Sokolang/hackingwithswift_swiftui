3232c2d7f3d302d89b43ad4b3d1dbed92f276701
Anzhelika Sokolova <63107259+Sokolang@users.noreply.github.com>
2021-01-28 21:19:04 +0200

    Add files via upload

---
+++ WordScramble/WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Anzhellika Sokolova on 14.01.2021.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
