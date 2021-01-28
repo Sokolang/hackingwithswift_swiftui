46659098c89b9e7ce6799812300bf88da1f43eeb
Anzhelika Sokolova <63107259+Sokolang@users.noreply.github.com>
2021-01-28 21:13:57 +0200

    Add files via upload

---
+++ Animations/Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Anzhellika Sokolova on 16.01.2021.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
