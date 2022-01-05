221e12f02ac5dd14d916830ac1909a85cf63837e
Anzhelika Sokolova <63107259+Sokolang@users.noreply.github.com>
2022-01-05 21:16:45 +0200

    Add files via upload

---
+++ Instafilter/Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by Anzhellika Sokolova on 05.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
